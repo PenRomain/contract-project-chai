e9a16cf5940c2b5934d41b42d93dcca497ee491a
Dmitrii <musatovit@gmail.com>
2023-01-25 15:22:25 +0300

    добавил в лейаут навбар и роут мейн

--- views/Layout.jsx
+++ views/Layout.jsx
@@ -1,4 +1,5 @@
 const React = require('react');
+const Navbar = require('./Navbar');
 
 module.exports = function Layout({ title, children }) {
   return (
@@ -9,7 +10,12 @@ module.exports = function Layout({ title, children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>{title}</title>
       </head>
-      <body>{children}</body>
+      <body>
+        <Navbar />
+        {' '}
+        {/* пропс,аунтификация или личный кабинет юзера или админа, и кнопка logout */}
+        {children}
+      </body>
     </html>
   );
 };
---
+++ views/Navbar.jsx
@@ -0,0 +1,10 @@
+const React = require('react');
+
+module.exports = function Log() {
+  return (
+    <div>
+      <div><a href="/auth/log">Login</a></div>
+      <div><a href="/auth/reg">Registration</a></div>
+    </div>
+  );
+};
