4b2d5f2684524f791adf006748d3efdfb929786c
Dmitrii <musatovit@gmail.com>
2023-01-26 17:16:23 +0300

    по нажатию на страну дает все чаи у этой страны

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 const Main = require('../views/Main');
 
+const { Country } = require('../db/models');
+const { Tea } = require('../db/models');
+
 router.get('/', (req, res) => {
   res.renderComponent(Main, { title: 'Карта чая' });
+});
+
+router.get('/:country', async (req, res) => {
+  try {
+    const country = req.params.country[0].toUpperCase() + req.params.country.substring(1);
+    const teaCountry = await Country.findAll({ where: { name: country }, include: Tea });
+    res.json(teaCountry); // res.renderComponent(View, {teaCountry})
+  } catch (e) {
+    res.status(500).json({ message: 'Что-то пошло не так' });
+  }
 });
 
 module.exports = router;
--- views/Main.jsx
+++ views/Main.jsx
@@ -5,12 +5,12 @@ module.exports = function Main({ title }) {
   return (
     <Layout title={title}>
       <div className="main_map">
-        <div className="china">Китай</div>
-        <div className="japan">Япония</div>
-        <div className="india">Индия</div>
-        <div className="vietnam">Вьетнам</div>
-        <div className="taiwan">Тайвань</div>
-        <div className="australia">Австралия</div>
+        <div className="china country" title="китайский"><a href="/china">Китай</a></div>
+        <div className="japan country"><a href="/japan">Япония</a></div>
+        <div className="india country"><a href="/india">Индия</a></div>
+        <div className="vietnam country"><a href="/vietnam">Вьетнам</a></div>
+        <div className="taiwan country"><a href="/taiwan">Тайвань</a></div>
+        <div className="australia country"><a href="/australia">Австралия</a></div>
         {/* <div className="china">china</div>
         <div className="japain">japain</div>
         <div className="india">india</div> */}
